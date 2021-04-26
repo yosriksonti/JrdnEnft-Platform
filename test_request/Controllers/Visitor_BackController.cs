1758ae68ec28f656c63c916523e2cc7f850a5490
Fedi Soltani <Soltanif6002@my.uwstout.edu>
2021-04-26 21:42:43 +0100

    Backend Visitor

---
+++ test_request/Controllers/Visitor_BackController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using test_request.Models.daycare;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test_request.Controllers
+{
+    public class Visitor_BackController : Controller
+    {
+        private REST rest = new REST();
+
+        // GET: /<controller>/
+        public IActionResult Visitors()
+        {
+            JArray response = rest.sendGetArrayRequest("http://127.0.0.1:8082/Visitors");
+            ViewBag.visitors = response;
+            return View();
+        }
+        public IActionResult Visitor(int ? id)
+        {
+            JObject response = rest.sendGetObjectRequest("http://127.0.0.1:8082/Visitor/"+id);
+            ViewBag.daycare = response;
+            return View();
+        }
+        public IActionResult Delete_Visitor(int? id)
+        {
+            HttpResponseMessage response = rest.sendDeleteRequest("http://127.0.0.1:8082/deleteVisitor/" + id);
+            return RedirectToAction("Visitors", "Visitor_Back");
+        }
+    }
+}
